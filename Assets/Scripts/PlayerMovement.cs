b81145042fad72cc4f85988226efa93a5137b79a
JMUCF <ju398578@ucf.edu>
2023-12-09 11:49:22 -0500

    Sprint stuff
    Added the sprinting functionality and a stamina bar that drains and refills. When John runs out of stamina it doesn't let you sprint for some time.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
+	public Slider sprint;
+	public Image sprintFill;
+	public float speedVar = 1f;
+	public float sprintAmount = 100f;
+	private bool tired = false;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
@@ -46,11 +52,42 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
+		
+		
+		if(Input.GetKey(KeyCode.LeftShift) && sprintAmount >= 0.1f && tired == false)
+		{
+			speedVar = 1.5f;
+			sprintAmount -= .7f;
+			sprint.value = sprintAmount;
+		}
+		else if(sprintAmount < 100f)
+		{
+			speedVar = 1f;
+			sprintAmount += .3f;
+			sprint.value = sprintAmount;
+		}
+		
+		if(sprintAmount <= 0.1f)
+		{
+			tired = true;
+		}
+		else if(sprintAmount >= 20 && tired == true)
+		{
+			tired = false;
+		}
+		
+		if(sprint.value <= 50)
+			sprintFill.color = Color.yellow;
+		if(sprint.value <= 20)
+			sprintFill.color = Color.red;
+		if(sprint.value >= 50)
+			sprintFill.color = Color.green;
+
     }
 
     void OnAnimatorMove ()
     {
-        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation (m_Rotation);
+			m_Rigidbody.MovePosition (m_Rigidbody.position + (m_Movement * speedVar) * m_Animator.deltaPosition.magnitude);
+			m_Rigidbody.MoveRotation (m_Rotation);
     }
 }
